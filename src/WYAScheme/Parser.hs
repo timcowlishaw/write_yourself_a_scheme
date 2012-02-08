58ed9ea083996fb1d4baea70e92ce7bf71503c19
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-02-08 11:38:40 +0000

    initial parser implementation

--- src/WYAScheme/Parser.hs
+++ src/WYAScheme/Parser.hs
@@ -1,2 +1,44 @@
 module WYAScheme.Parser where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
 
+data LispVal  = Atom String
+              | List [LispVal]
+              | DottedList [LispVal] LispVal
+              | Number Integer
+              | String String
+              | Bool Bool
+              deriving (Show, Eq)
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many $ noneOf "\""
+  char '"'
+  return $ String x
+  
+parseAtom :: Parser LispVal
+parseAtom = do 
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+readExpr :: String -> Either ParseError LispVal
+readExpr = parse parseExpr "lisp"
