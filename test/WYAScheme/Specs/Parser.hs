99b5a01ae620227d032cd1fa8f68cf7312115f9b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-02-08 16:24:44 +0000

    specs for parser 1 question 4 - parsing different number base representations

--- test/WYAScheme/Specs/Parser.hs
+++ test/WYAScheme/Specs/Parser.hs
@@ -11,34 +11,34 @@ instance Eq ParseError where
 parserSpecs :: [Spec]
 parserSpecs = describe "The parser" [
     it "parses Strings delimited by double quotes" (
-      (Right (String "Hello there")) == (readExpr "\"Hello there\"")
+      (Right (String "Hello there")) == readExpr "\"Hello there\""
     ),
     it "parses Strings containing an escaped double quote" (
-      (Right (String "Hello, so-called \"Haskell Programmer\"")) == (readExpr "\"Hello, so-called \\\"Haskell Programmer\\\"\"")
+      (Right (String "Hello, so-called \"Haskell Programmer\"")) == readExpr "\"Hello, so-called \\\"Haskell Programmer\\\"\""
     ),
     it "parses Strings containing an escaped tab" (
-      (Right (String "Words\tseparated\tby\ttabs")) == (readExpr "\"Words\\\tseparated\\\tby\\\ttabs\"")
+      (Right (String "Words\tseparated\tby\ttabs")) == readExpr "\"Words\\\tseparated\\\tby\\\ttabs\""
     ),
     it "parses Strings containing an escaped newline" (
-      (Right (String "Words\nseparated\nby\nnewlines")) == (readExpr "\"Words\\\nseparated\\\nby\\\nnewlines\"")
+      (Right (String "Words\nseparated\nby\nnewlines")) == readExpr "\"Words\\\nseparated\\\nby\\\nnewlines\""
     ),
     it "parses Strings containing an escaped carriage return" (
-      (Right (String "Words\rseparated\rby\rcarriage\rreturns")) == (readExpr "\"Words\\\rseparated\\\rby\\\rcarriage\\\rreturns\"")
+      (Right (String "Words\rseparated\rby\rcarriage\rreturns")) == readExpr "\"Words\\\rseparated\\\rby\\\rcarriage\\\rreturns\""
     ),
     it "parses Strings containing an escaped backslash" (
-      (Right (String "Words\\separated\\by\\backslashes")) == (readExpr "\"Words\\\\separated\\\\by\\\\backslashes\"")
+      (Right (String "Words\\separated\\by\\backslashes")) == readExpr "\"Words\\\\separated\\\\by\\\\backslashes\""
     ),
     it "parses atoms beginning with a letter" (
-      (Right (Atom "hello")) == (readExpr "hello")
+      (Right (Atom "hello")) == readExpr "hello"
     ),
     it "parses atoms beginning with a symbol" (
-      (Right (Atom "#hello")) == (readExpr "#hello")
+      (Right (Atom "#hello")) == readExpr "#hello"
     ),
     it "parses atoms containing symbols" (
-      (Right (Atom "hello?world")) == (readExpr "hello?world")
+      (Right (Atom "hello?world")) == readExpr "hello?world"
     ),
     it "parses atoms containing numbers" (
-      (Right (Atom "hello2world")) == (readExpr "hello2world")
+      (Right (Atom "hello2world")) == readExpr "hello2world"
     ),
     it "parses the atom #t as a boolean true value" (
       (Right (Bool True)) == readExpr "#t"
@@ -48,4 +48,16 @@ parserSpecs = describe "The parser" [
     ),
     it "parses integers" (
       (Right (Number 12345)) == readExpr "12345"
+    ),
+    it "parses binary representations of integers" (
+      (Right (Number 12345)) == readExpr "#b11000000111001"
+    ),
+    it "parses octal representations of integers" (
+        (Right (Number 12345)) == readExpr "#o30071"
+    ),
+    it "parses decimal representations of integers" (
+        (Right (Number 12345)) == readExpr "#d12345"
+    ),
+    it "parses hexadecimal representations of integers" (
+        (Right (Number 12345)) == readExpr "#h3039"
     )]
